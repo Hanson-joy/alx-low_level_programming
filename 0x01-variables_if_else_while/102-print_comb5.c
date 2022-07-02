9db6f6fd5ae809b7d98ea2280e884c73409a8a8b
Hanson-joy <hansonjoy53@gmail.com>
2022-07-02 02:30:37 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,47 +1,30 @@
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
 /**
-*main-Entry point
-*Return: Always 0 (Success)
+*main-prints all possible combinations of two two-digit numbers.
+*Return: 0
 */
 int main(void)
 {
-int p = 48, q = 48, y = 48, x = 49;
-while (p < 58)
+int i;
+int j;
+for (i = 0; i < 100; i++)
 {
-while (q < 58)
+for (j = 0; j < 100; j++)
 {
-while (y < 58)
+if (j > i)
 {
-while (x < 58)
-{
-putchar(p);
-putchar(q);
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
 putchar(' ');
-putchar(y);
-putchar(x);
-if (!(p == 57 &&
-q == 56 &&
-y == 57 &&
-x == 57))
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (i * 100 + j != 9899)
 {
 putchar(',');
 putchar(' ');
 }
-x++;
 }
-y++;
-x = 48;
 }
-q++;
-q = p;
-x = q + 1;
-}
-p++;
-q = 48;
-y = p;
-x = q + 1;
 }
 putchar('\n');
 return (0);
