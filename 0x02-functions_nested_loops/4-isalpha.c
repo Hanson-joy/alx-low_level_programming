1d437f564af0b733225c3c26377dd9a2c157accd
Hanson-joy <hansonjoy53@gmail.com>
2022-07-06 02:44:08 -0700

    git commit -m "4-isalpha"

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,33 +1,10 @@
 #include "main.h"
-
 /**
-
-* _isalpha - Return 1 if c is a letter. lowercase or uppercase
-
-* @c: The int to print
-
-* Return: Always 0.
-
+*_isalpha-check for lower case letter
+*@c:character to check
+*Return: 0 or 1
 */
-
 int _isalpha(int c)
-
 {
-
-if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
-
-{
-
-return (1);
-
-}
-
-else
-
-{
-
-return (0);
-
-}
-
+return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
 }
