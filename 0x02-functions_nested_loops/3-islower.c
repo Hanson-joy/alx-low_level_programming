52ee57ce6339ac1a2c6f623ed931e6e75c714c2f
Hanson-joy <hansonjoy53@gmail.com>
2022-07-05 17:47:29 -0700

    3 is lower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,8 @@
 #include "main.h"
 /**
-<<<<<<< HEAD
 *_islower - Short description, single line
 *@c: contains value to be compared
-*Return: Always 0
-=======
-* _islower - Short description, single line
-* @c: contains value to be compared
 * Return: Always 0.
->>>>>>> db2d810c40d17c0d4a4b1aaa03527798bba76b69
 */
 int _islower(int c)
 {
