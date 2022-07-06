fe80462fd34e9d39dfe203a42c0a4f33eff83246
Hanson-joy <hansonjoy53@gmail.com>
2022-07-05 17:13:10 -0700

    3 islower.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,3 +10,9 @@ void print_alphabet_x10(void);
 /**
 *_islower - checks for lowercase
 */
+int _islower(int c);
+/**
+* _isalpha-checks for alphabet character
+*@c: character to check
+*Return: 1 if lower or uppercase, 0 if not alphabet character
+*/
