c5a779df136d2b915905f6c6008c2e0f016b9e50
Hanson-joy <hansonjoy53@gmail.com>
2022-07-07 18:20:57 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*mul - takes 2 variable and multiply them
+*@var1: first variable
+*@var2: second variable
+*Return: var1 * var2
+*/
+int mul(int var1, int var2)
+{
+return (var1 * var2);
+}
