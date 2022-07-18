4ec75a31b605f516d083a454cb1671bb92627e82
Hanson-joy <106768228+Hanson-joy@users.noreply.github.com>
2022-07-18 14:05:05 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -23,4 +23,22 @@ unsigned int _strspn(char *s, char *accept)
 		j = 0;
 		while (str2[j] != '\0') /*Declaring WHILE *accept*/
 		{
-			if (str2[j] == str1[i]
+			if (str2[j] == str1[i]/*Evaluate condition*/
+			{
+				count++; /*count number*/
+				break;
+			}
+
+			j++;    /*add j+1*/
+		}
+
+		if (s[i] != accept[j]) /*If aren't equals*/
+		{
+			break;
+		}
+
+		i++; /*add x+1*/
+	}
+
+	return (count); /*return the value of count*/
+}
