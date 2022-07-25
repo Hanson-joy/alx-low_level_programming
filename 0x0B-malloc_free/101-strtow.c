95ed8041b1e5377898d0221f061b0045f2403316
Hanson-joy <106768228+Hanson-joy@users.noreply.github.com>
2022-07-25 18:52:44 +0100

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,34 +1,70 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
-* argstostr - Concatenates all arguments of the program into a string;
-* arguments are separated by a new line in the string.
-* @ac: The number of arguments passed to the program.
-* @av: An array of pointers to the arguments.
-* Return: If ac == 0, av == NULL, or the function fails - NULL.
-* Otherwise - a pointer to the new string.
-*/
-char *argstostr(int ac, char **av)
+ * ch_free_grid - frees a 2 dimensional array.
+ * @grid: multidimensional array of char.
+ * @height: height of the array.
+ * Return: no return
+ */
+
+void ch_free_grid(char **grid, unsigned int height)
+
 {
-char *str;
-int arg, byte, index, size = ac;
-if (ac == 0 || av == NULL)
-return (NULL);
-for (arg = 0; arg < ac; arg++)
+if (grid != NULL && height != 0)
 {
-for (byte = 0; av[arg][byte]; byte++)
-size++;
+for (; height > 0; height--)
+free(grid[height]);
+free(grid[height]);
+free(grid);
 }
-str = malloc(sizeof(char) * size + 1);
-if (str == NULL)
-return (NULL);
-index = 0;
-for (arg = 0; arg < ac; arg++)
-{
-for (byte = 0; av[arg][byte]; byte++)
-str[index++] = av[arg][byte];
-str[index++] = '\n';
 }
-str[size] = '\0';
-return (str);
+
+/**
+ * strtow - splits a string into words.
+ * @str: string.
+ *
+ * Return: pointer of an array of integers
+ */
+
+char **strtow(char *str)
+
+{
+char **aout;
+unsigned int c, height, i, j, a1;
+
+if (str == NULL || *str == '\0')
+return (NULL);
+for (c = height = 0; str[c] != '\0'; c++)
+if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+height++;
+aout = malloc((height + 1) * sizeof(char *));
+if (aout == NULL || height == 0)
+{
+free(aout);
+return (NULL);
+}
+for (i = a1 = 0; i < height; i++)
+{
+for (c = a1; str[c] != '\0'; c++)
+{
+if (str[c] == ' ')
+a1++;
+if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+{
+aout[i] = malloc((c - a1 + 2) * sizeof(char));
+if (aout[i] == NULL)
+{
+ch_free_grid(aout, i);
+return (NULL);
+}
+break;
+}
+}
+for (j = 0; a1 <= c; a1++, j++)
+aout[i][j] = str[a1];
+aout[i][j] = '\0';
+}
+aout[i] = NULL;
+return (aout);
 }
